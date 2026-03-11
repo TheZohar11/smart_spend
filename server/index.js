f51b14e3ecc0c971bd0a3ddc7747e59e72e1724f
michaelkupfer97 <michaelkulhs@gmail.com>
2026-03-12 01:41:48 +0200

    connect to MongoDB + save secrets in .env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 //sever code here
 import express from "express";
 import dotenv from "dotenv";
-import MongoClient from "mongodb";
+import { MongoClient } from "mongodb";
 
 dotenv.config();
 
@@ -15,3 +15,27 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// mongoDB atlas connection string (from .env)
+const url = process.env.MONGODB_URI;
+if (!url) {
+  throw new Error("Missing MONGODB_URI in .env ");
+}
+const client = new MongoClient(url);
+
+// db name 
+const dbName = "smart_spend";
+
+async function main() {
+  try {
+    //connect to atlas database
+    await client.connect();
+    console.log("Connected to MongoDB Atlas");
+  
+  } catch (error) {
+    console.error("Error connecting to MongoDB Atlas", error);
+  }
+
+}
+
+main().catch(console.error);
