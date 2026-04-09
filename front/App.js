bd88624ff73eef479bd86bffb6f8772d48b5abdd
zohar <zoharsim2000@gmail.com>
2026-04-09 13:58:21 +0300

    adding a tab bar for home screen

--- front/App.js
+++ front/App.js
@@ -3,18 +3,58 @@ import { registerRootComponent } from "expo";
 import HomeScreen from "./app/screens/HomeScreen";
 import LandingScreen from "./app/screens/LandingScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
+import ProfileScreen from "./app/screens/ProfileScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <BottomTab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarStyle: { position: "absolute" },
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === "Home") {
+            iconName = "home-outline";
+          } else if (route.name === "Profile") {
+            iconName = "person-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <BottomTab.Screen name="Home" component={HomeScreen} />
+      <BottomTab.Screen name="Profile" component={ProfileScreen} />
+    </BottomTab.Navigator>
+  );
+}
 
 function RootStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Landing" component={LandingScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen
+        name="Landing"
+        component={LandingScreen}
+          options={{ headerShown: true, headerLeft: () => null }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{ headerShown: true }}
+      />
     </Stack.Navigator>
   );
 }
@@ -30,3 +70,12 @@ export default function App() {
 }
 
 registerRootComponent(App);
+
+const styles = StyleSheet.create({
+  tabStyle: {},
+  tabBarLabelStyle: {
+    fontSize: 16,
+    fontFamily: "Georgia",
+    fontWeight: 300,
+  },
+});
